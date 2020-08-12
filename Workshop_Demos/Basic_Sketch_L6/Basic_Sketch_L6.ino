1c94dca3305a04081bd74b3b5515a474b40e130d
M.Ratcliffe <mikemx50@googlemail.com>
2020-08-12 01:42:44 +0100

    Add files via upload

--- Workshop_Demos/Basic_Sketch_L6/Basic_Sketch_L6.ino
+++ Workshop_Demos/Basic_Sketch_L6/Basic_Sketch_L6.ino
@@ -19,6 +19,8 @@
 
 -Basic_Sketch_L5: The esp8266 has iot capabilities lets have a web based readout
 
+-Basic_Sketch_L6: The esp8266 also has 4mb of internal Non-volatile memory, we can use this to store data locally in the form of a sensor data log and html files to make the gui more interactive
+
   
 */
 
@@ -26,16 +28,118 @@
 // IOT Variables
 ///////////////////////////////////////////////////////////////////////////////
 #include <ESP8266WiFi.h>
+#include <FS.h>   
 const char* ssid = "Farm-B";//type your ssid
 const char* password = "";//type your password
+String Filepath="/Log/data1.csv"; // Thids is the path to where we are storing our data
+File Logfile;
+File fsUploadFile;
+String DataLog=""; // Just a string that we use to store data temp before upload
 
-WiFiServer server(80);//Service Port
+
+//WiFiServer server(80);//Service Port
+#include <WebSocketsServer.h>
+#include <WebSocketsClient.h>
+WebSocketsServer webSocket = WebSocketsServer(81); //setting up websockets stuff for sending user setpoints
+//WebSocketsClient webSocket;
+int WebSocketsReadings=0;  //A Flag For When To Send Data T readings.html via websckets                           
+
+
+#include <ESP8266WebServer.h>
+ESP8266WebServer server(80);
+
+
+
+
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  //Some extra stuff t handle the gzipped cntent [faster page load]
+else if(filename.endsWith(".htm.gz")) return "text/html";
+else if(filename.endsWith(".html.gz")) return "text/html";
+else if(filename.endsWith(".css.gz")) return "text/css";
+else if(filename.endsWith(".js.gz")) return "application/javascript";
+else if(filename.endsWith(".png.gz")) return "image/png";
+else if(filename.endsWith(".gif.gz")) return "image/gif";
+else if(filename.endsWith(".jpg.gz")) return "image/jpeg";
+else if(filename.endsWith(".ico.gz")) return "image/x-icon";
+else if(filename.endsWith(".xml.gz")) return "text/xml";
+else if(filename.endsWith(".pdf.gz")) return "application/x-pdf";
+else if(filename.endsWith(".zip.gz")) return "application/x-zip";
+else if(filename.endsWith(".gz")) return "application/x-gzip";
+
+
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".csv")) return "text/csv";
+
+//Extra Stuff to handle fnt types
+else if(filename.endsWith(".woff")) return  "application/x-font-woff";
+else if(filename.endsWith(".woff2")) return  "application/octet-stream";
+else if(filename.endsWith(".ttf")) return   "application/font-ttf";
+else if(filename.endsWith(".eot")) return   "application/vnd.ms-fontobject";
+else if(filename.endsWith(".otf")) return   "application/font-otf";
+ 
+
+  return "application/x-gzip";
+ //return "text/plain";
+
+
+ 
+}
+bool handleFileRead(String path){
+  if(path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleFileList() {
+  if(!server.hasArg("dir")) {server.send(500, "text/plain", "BAD ARGS"); return;}
+  
+  String path = server.arg("dir");
+  Dir dir = SPIFFS.openDir(path);
+  path = String();
+
+  String output = "[";
+  while(dir.next()){
+    File entry = dir.openFile("r");
+    if (output != "[") output += ',';
+    bool isDir = false;
+    output += "{\"type\":\"";
+    output += (isDir)?"dir":"file";
+    output += "\",\"name\":\"";
+    output += String(entry.name()).substring(1);
+    output += "\"}";
+    entry.close();
+  }
+  
+  output += "]";
+  server.send(200, "text/json", output);
+}
+
+
 //*****************************************************************************
 // Enviromental variables
 ///////////////////////////////////////////////////////////////////////////////
 float Set_Temp = 22; // How hot we want the max temp of the greenhouse
 float Bandgap =0.5;
-
 
 //*****************************************************************************
 // Libs, variables and info pertaining to the Servo controlling windows
@@ -87,10 +191,6 @@ void setup() {
   Read_BME();
   delay(100);
 
-  Serial.println(F("Setting up IOT"));
-  IOT_Setup();
-  delay(100);
-
   Serial.println(F("Setting up Servo"));
   Window_Servo.attach(D8);  // attaches the servo on GIO2 to the servo object
   Test_Servo(); // When system is powered on, run the servo to indicate to user its working
@@ -99,6 +199,38 @@ void setup() {
   Serial.println(F("Setting up IOT"));
   IOT_Setup();
   delay(100);
+  SetupWebSokets(); // how we are passing data between esp8266 and html webpage
+  
+
+  
+    {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {    
+    String fileName = dir.fileName();
+     size_t fileSize = dir.fileSize();
+    }
+  }
+  
+  //SERVER INIT
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+   delay(5);
+  //load editor
+  server.on("/edit", HTTP_GET, [](){
+    if(!handleFileRead("/edit.html")) server.send(404, "text/plain", "FileNotFound");
+  });
+   delay(5);
+  //create file
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([](){
+    if(!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
+   delay(5);
+
+ 
 }
 
 //**********************************************************************************************************************************************************
@@ -113,8 +245,10 @@ Serial.println(millis());
 Serial.println(F("up-Time:"));
 Serial.println(millis());
 Read_BME();
+LogData();
 Control_Temp_V1();
-Check_IOT();
+//Check_IOT();
+Run_IOT();
 Serial.print("Use this URL to connect: ");
 Serial.print("http://");
 Serial.print(WiFi.localIP());
@@ -290,19 +424,27 @@ Serial.print("Use this URL to connect: ");
 Serial.print("http://");
 Serial.print(WiFi.localIP());
 Serial.println("/");
+
+SPIFFS.begin();// We need to boot up the internal memory
 }
 
+void SetupWebSokets(){
+    webSocket.begin();
+    delay(40);
+    webSocket.onEvent(webSocketEvent);
+    yield();
+    delay(40);
+    
+ }
+
 //**********************************************************************************************************************************************************
-//  Very basic IOT, reconnecting if connection lost and sending user some data 
+//  If a user connects to the IP of the controller, send them a html file from the internal memory and update their sensor readings
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void Check_IOT(){
-  
 
-
+void Run_IOT() {//Run IOT things multiple time to reduce lag?
 if (WiFi.status() != WL_CONNECTED){
 if(sizeof(password)>0)WiFi.begin(ssid,password);
 else WiFi.begin(ssid);
-delay(2000);
 server.begin();
 }
 else Serial.println("WiFi connected");
@@ -313,46 +455,159 @@ Serial.print("Use this URL to connect: ");
 Serial.print("http://");
 Serial.print(WiFi.localIP());
 Serial.println("/");
-// Check if a client has connected
-WiFiClient client = server.available();
-if (client) {
+for (int count=0; count <= 20; count++){
+      delay(1);
+       yield(); //Yield Is a ESP8266 Function, it gives processing time to the hidden functions needed to run IOT,Spiffs etc 
+      server.handleClient(); //delivering webpage to user
+      delay(1);
+       yield();
+    webSocket.loop();//checking if we have new data from website
+      delay(1);
+      yield();
+}
+}
 
-Serial.println("new client");
+//*********************** Websockets Code For Moving data too and from GUI ****************************//
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
 
+    switch(type) {
+        case WStype_DISCONNECTED:
+            
+            break;
+            //Sending some stuff to the webgui
+        case WStype_CONNECTED:
+         {  Serial.println(F("WebSockets Connected"));
+          IPAddress ip = webSocket.remoteIP(num);
+          //Sending each bit of data one at a time, maybe it would be cleaner to json this up at a later date 
+         webSocket.sendTXT(num, "WeBSocketsConnected");
+       
+          }
+         
+       
+            break;
+        case WStype_TEXT:
+        {
+           String text = String((char *) &payload[0]); //Turning the websockets incoming into a string
 
-
-// Return the response
-client.println("HTTP/1.1 200 OK");
-client.println("Content-Type: text/html");
-client.println(""); //  do not forget this one
-client.println("<!DOCTYPE HTML>");
-client.println("<html>");
-
-    client.print("Temperature = ");
-    client.print(Air_temp);
-    client.println(" *C");
-
-client.println("<br><br>");
-
-    client.print("Pressure = ");
-    client.print(Pressure);
-    client.println(" hPa");
-
-client.println("<br><br>");
-
-    client.print("Humidity = ");
-    client.print(Humidity);
-    client.println(" %");
-
-client.println("<br><br>");
-    
-    client.print("VPD = ");
-    client.print(VPD);
-    client.println("kPa");
-
-
-client.println("<br><br>");
-client.println("</html>");
+//************************** IF Reading HTML Is Open **************************************************************//
+if(text.startsWith("#ConnectedReadings#")){  //Html is sendig "Connected" when any index page is loaded
+webSocket.sendTXT(num, "Connected To Reading WebSockets");                                
+Serial.println("Readings WebSockets Initiated");
+}
+if(text.startsWith("#SendMeSomeData#")){  //Html wants some more data now and it has been 5 seconds or more since we last sent some
+Read_BME();//Updating the Readings for the GUI
+Serial.println("Readings WebSockets Ended");  
+//Checking if the payload came from Setpoints GUI
+Serial.println("Updated Readings html");
+String t= {"#Time#"+String(millis())};
+webSocket.sendTXT(num, t);
+delay(20);
+String a= {"#S1#"+String(Air_temp)};
+webSocket.sendTXT(num, a);
+delay(20);
+String b= {"#S2#"+String(Humidity)};
+webSocket.sendTXT(num, b);
+delay(20);
+String c= {"#S3#"+String(VPD)};
+webSocket.sendTXT(num, c);
+     
 
 }
+
+if(text.startsWith("#DeleetLog#")){  //Html is sendig "Connected" when any index page is loaded
+                                 DeleetLog();
+                                 webSocket.sendTXT(num, "#LogDeleeted#");
+                                 Serial.println(F("Deleeted Log Files"));
+                               }
+
+
+            }
+            
+           Serial.println(F("Reached end of websockets call"));
+           webSocket.sendTXT(num, payload, lenght);  //# Dont think we need this m.r
+            webSocket.broadcastTXT(payload, lenght);  //# Dont think we need this m.r
+           delay(2);
+            break;
+
+        //# Need to refresh my memory on what thi is actually doing? m.r
+        case WStype_BIN:
+     
+            hexdump(payload, lenght);
+
+            // echo data back to browser
+            webSocket.sendBIN(num, payload, lenght);
+            break;
+
+
+            
+    }
+
+
+}
+
+
+//**********************************************************************************************************************************************************
+//  Logging data to the internal memory
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////     
+/*Useful Stuff:
+
+   r      Open text file for reading.  The stream is positioned at the
+          beginning of the file.
+
+   r+     Open for reading and writing.  The stream is positioned at the
+          beginning of the file.
+
+   w      Truncate file to zero length or create text file for writing.
+          The stream is positioned at the beginning of the file.
+
+   w+     Open for reading and writing.  The file is created if it does
+          not exist, otherwise it is truncated.  The stream is
+          positioned at the beginning of the file.
+
+   a      Open for appending (writing at end of file).  The file is
+          created if it does not exist.  The stream is positioned at the
+          end of the file.
+
+   a+     Open for reading and appending (writing at end of file).  The
+          file is created if it does not exist.  The initial file
+          position for reading is at the beginning of the file, but
+          output is always appended to the end of the file.
+*/
+
+//** We are creatng a .csv format  "data"+","+"data2"+","+"data3"
+void LogData(){
+Serial.print(F("Logging Data to Spiffs Log File:"));  
+
+if (Logfile.size()>=1000000){ // How Much memory we are allocatng to the logs in bytes [100,000 = 0.1mb])){ // Lets not over run our memory, strange things will happen
+Serial.println(F("LogFile Full, not logging data"));
+Logfile.close();
+           
+                                }
+
+// If memory isnt full, lets fill it up with data readings
+else {
+
+Logfile = SPIFFS.open(Filepath, "a");// Open the log file in write mode
+
+DataLog=Air_temp; //A 
+DataLog+=","; //A 
+DataLog+=Humidity; //A 
+DataLog+=","; //A 
+DataLog+=VPD; //A 
+Serial.println(DataLog);
+Logfile.println(DataLog); // commiting the new cvs data string to memory
+delay(10);
+Logfile.close();
+ 
+  
+}
+}
+
+void  DeleetLog(){
+  Serial.println("User Selected to Deleet data log");
+ File Logfile = SPIFFS.open("/Log/data1.csv", "w");    
+   Logfile.println("Temp *C, Humidity %, VPD kPa");  //puttng some blank data in   
+   Logfile.close();
+
+  
 }
